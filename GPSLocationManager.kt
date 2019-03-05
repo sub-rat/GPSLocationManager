b8b57e8cb06d64e4c1e1de532fa470f2011e46c5
Subrat Gyawali <subrat.gyawali@gmail.com>
2019-03-06 05:19:52 +0545

    Update GPSLocationManager.kt

--- GPSLocationManager.kt
+++ GPSLocationManager.kt
@@ -1,105 +1,306 @@
+
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
-import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.location.LocationManager
+import android.location.Location
+import android.net.Uri
+import android.os.Looper
+import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
+import com.rosia.BuildConfig
+import java.text.DateFormat
+import java.util.*
+
+class GPSLocationManager2(var context: Activity, var callback: GPSLocationManager2.GPSLocationCallback) : LifecycleObserver {
+    /**
+     * Code used in requesting runtime permissions.
+     */
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+
+    /**
+     * Constant used in the location settings dialog.
+     */
+    private val REQUEST_CHECK_SETTINGS = 0x1
+
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    private val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
+
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    private val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2
+
+    // Keys for storing activity state in the Bundle.
+    private val KEY_REQUESTING_LOCATION_UPDATES = "requesting-location-updates"
+    private val KEY_LOCATION = "location"
+    private val KEY_LAST_UPDATED_TIME_STRING = "last-updated-time-string"
+
+    /**
+     * Provides access to the Fused Location Provider API.
+     */
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+
+    /**
+     * Provides access to the Location Settings API.
+     */
+    private var mSettingsClient: SettingsClient? = null
+
+    /**
+     * Stores parameters for requests to the FusedLocationProviderApi.
+     */
+    private var mLocationRequest: LocationRequest? = null
+
+    /**
+     * Stores the types of location services the client is interested in using. Used for checking
+     * settings to determine if the device has optimal location settings.
+     */
+    private var mLocationSettingsRequest: LocationSettingsRequest? = null
+
+    /**
+     * Callback for Location events.
+     */
+    private var mLocationCallback: LocationCallback? = null
+
+    /**
+     * Represents a geographical location.
+     */
+    private var mCurrentLocation: Location? = null
 
 
-/**
- *
- */
-class GPSLocationManager constructor(var context: Activity, var callback: GPSLocationCallback) : LifecycleObserver {
+    /**
+     * Tracks the status of the location updates request. Value changes when the user presses the
+     * Start Updates and Stop Updates buttons.
+     */
+    private var mRequestingLocationUpdates: Boolean? = null
 
+    /**
+     * Time when the location was updated represented as a String.
+     */
+    private var mLastUpdateTime: String? = null
 
-    lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
-    lateinit var mLocationCallback: LocationCallback
-
-    private val REQUEST_LOCATION_PERMISSION = 1
-    private var mTrackingLocation: Boolean = false
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onStart() {
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+        mRequestingLocationUpdates = false
+        mLastUpdateTime = ""
+
+        // Update values using data stored in the Bundle.
+//        updateValuesFromBundle(savedInstanceState)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+        mSettingsClient = LocationServices.getSettingsClient(context)
+
+        // Kick off the process of building the LocationCallback, LocationRequest, and
+        // LocationSettingsRequest objects.
+        createLocationCallback()
+        createLocationRequest()
+        buildLocationSettingsRequest()
+    }
 
 
+    /**
+     * Sets up the location request. Android has two location request settings:
+     * `ACCESS_COARSE_LOCATION` and `ACCESS_FINE_LOCATION`. These settings control
+     * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
+     * the AndroidManifest.xml.
+     *
+     *
+     * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
+     * interval (5 seconds), the Fused Location Provider API returns location updates that are
+     * accurate to within a few feet.
+     *
+     *
+     * These settings are appropriate for mapping applications that show real-time location
+     * updates.
+     */
+    fun createLocationRequest() {
+        mLocationRequest = LocationRequest()
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest?.interval = UPDATE_INTERVAL_IN_MILLISECONDS
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest?.fastestInterval = FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS
+
+        mLocationRequest?.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+
+    /**
+     * Creates a callback for receiving location events.
+     */
+    fun createLocationCallback() {
         mLocationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult?) {
-                Log.d("locationUpdate", "${locationResult?.lastLocation?.latitude}")
-                callback.getLocationUpdate(locationResult?.lastLocation?.latitude.toString(), locationResult?.lastLocation?.longitude.toString())
-            }
-        }
-        if (checkGpsStatus())
-            startTrackingLocation()
-    }
-
-    private fun checkGpsStatus(): Boolean {
-
-        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-    }
-
-    fun startTrackingLocation() {
-        if (ActivityCompat.checkSelfPermission(context,
-                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(context, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    REQUEST_LOCATION_PERMISSION)
-        } else {
-            if (checkGpsStatus()) {
-                mTrackingLocation = true
-                mFusedLocationProviderClient.requestLocationUpdates(getLocationRequest(),
-                        mLocationCallback,
-                        null /* Looper */)
-            } else {
-                Toast.makeText(context, "Enable your gps", Toast.LENGTH_SHORT).show()
+                super.onLocationResult(locationResult)
+                mCurrentLocation = locationResult!!.lastLocation
+                mLastUpdateTime = DateFormat.getTimeInstance().format(Date())
+                callback.getLocationUpdate(mCurrentLocation)
             }
         }
     }
 
-    private fun getLocationRequest(): LocationRequest? {
-        var locationRequest = LocationRequest.create()
-        locationRequest.interval = 10000
-        locationRequest.fastestInterval = 5000
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        return locationRequest
+
+    /**
+     * Uses a [com.google.android.gms.location.LocationSettingsRequest.Builder] to build
+     * a [com.google.android.gms.location.LocationSettingsRequest] that is used for checking
+     * if a device has the needed location settings.
+     */
+    fun buildLocationSettingsRequest() {
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(mLocationRequest!!)
+        mLocationSettingsRequest = builder.build()
     }
+
+
+    /**
+     * Requests location updates from the FusedLocationApi. Note: we don't call this unless location
+     * runtime permission has been granted.
+     */
+    @SuppressLint("MissingPermission")
+    fun startLocationUpdates() {
+        // Begin by checking if the device has the necessary location settings.
+        mSettingsClient?.checkLocationSettings(mLocationSettingsRequest)?.addOnSuccessListener {
+            mFusedLocationClient?.requestLocationUpdates(mLocationRequest,
+                    mLocationCallback, Looper.myLooper())
+        }?.addOnFailureListener {
+            val statusCode = (it as ApiException).statusCode
+            when (statusCode) {
+                LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                    Log.i("LocationManager", "Location settings are not satisfied. Attempting to upgrade " + "location settings ")
+                    try {
+                        // Show the dialog by calling startResolutionForResult(), and check the
+                        // result in onActivityResult().
+                        val rae = it as ResolvableApiException
+                        rae.startResolutionForResult(context, REQUEST_CHECK_SETTINGS)
+                    } catch (sie: IntentSender.SendIntentException) {
+                        Log.i("LocationManager", "PendingIntent unable to execute request.")
+                    }
+
+                }
+                LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                    val errorMessage = "Location settings are inadequate, and cannot be " + "fixed here. Fix in Settings."
+                    Log.e("LocationManager", errorMessage)
+                    Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show()
+                    mRequestingLocationUpdates = false
+                }
+            }
+        }
+
+    }
+
+
+    /**
+     * Removes location updates from the FusedLocationApi.
+     */
+    private fun stopLocationUpdates() {
+        if (!mRequestingLocationUpdates!!) {
+            Log.d("LocationManager", "stopLocationUpdates: updates never requested, no-op.")
+            return
+        }
+
+        // It is a good practice to remove location requests when the activity is in a paused or
+        // stopped state. Doing so helps battery performance and is especially
+        // recommended in applications that request frequent location updates.
+        mFusedLocationClient?.removeLocationUpdates(mLocationCallback)?.addOnCompleteListener {
+            mRequestingLocationUpdates = false
+        }
+    }
+
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause() {
-        if (mTrackingLocation) {
-            stopTrackingLocation()
-            mTrackingLocation = true
-            mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
-        }
-
+        // Remove location updates to save battery.
+        stopLocationUpdates()
     }
-
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
-        if (mTrackingLocation) {
-            startTrackingLocation()
-        }
-
-    }
-
-    private fun stopTrackingLocation() {
-        if (mTrackingLocation) {
-            mTrackingLocation = false
+        if (!mRequestingLocationUpdates!! && checkPermissions()) {
+            startLocationUpdates()
+            mRequestingLocationUpdates=true
+        } else if (!checkPermissions()) {
+            requestPermissions()
         }
     }
 
 
     interface GPSLocationCallback {
 
-        fun getLocationUpdate(latitude: String?, longitude: String?)
+        fun getLocationUpdate(location: Location?)
 
     }
 
+    /**
+     * Return the current state of the permissions needed.
+     */
+    private fun checkPermissions(): Boolean {
+        val permissionState = ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+        return permissionState == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(context,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_PERMISSIONS_REQUEST_CODE)
+    }
+
+    fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray) {
+
+        if (requestCode == 1) {
+            when {
+                grantResults.isEmpty() -> {
+                    Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+                (grantResults[0] == PackageManager.PERMISSION_GRANTED) -> {
+                    startLocationUpdates()
+                }
+
+                else -> {
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    val uri = Uri.fromParts("package",
+                            BuildConfig.APPLICATION_ID, null)
+                    intent.data = uri
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    context.startActivity(intent)
+                }
+            }
+        }
+    }
+
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+        // Check for the integer request code originally supplied to startResolutionForResult().
+            REQUEST_CHECK_SETTINGS -> when (resultCode) {
+                Activity.RESULT_OK -> Log.i("GPSLocationManager", "User agreed to make required location settings changes.")
+                Activity.RESULT_CANCELED -> {
+                    Log.i("GPSLocationManager", "User chose not to make required location settings changes.")
+                    mRequestingLocationUpdates = false
+                }
+            }// Nothing to do. startLocationupdates() gets called in onResume again.
+        }
+    }
+
+}
